e5829eda98670126170d3eabe089879b6fa62a85
Marek Tews <marek@trx.com.pl>
2023-05-05 10:18:13 +0200

    CameraX - preview and image analysis

--- app/src/main/java/mt/parkingqr/ui/home/HomeFragment.kt
+++ app/src/main/java/mt/parkingqr/ui/home/HomeFragment.kt
@@ -4,39 +4,90 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import mt.parkingqr.databinding.FragmentHomeBinding
+import java.lang.Exception
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class HomeFragment : Fragment() {
+    private var viewBinding: FragmentHomeBinding? = null
+    private lateinit var cameraExecutor: ExecutorService
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        viewBinding = FragmentHomeBinding.inflate(inflater, container, false).apply {
+            homeViewModel.text.observe(viewLifecycleOwner) {
+                textHome.text = it
+            }
+            startCamera(this)
         }
-        return root
+
+        return viewBinding!!.root
+    }
+
+    private fun startCamera(binding: FragmentHomeBinding) {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+            // Which camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            // Image analysis
+            val imageAnalysis = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, ImageAnalysis.Analyzer { imageProxy: ImageProxy ->
+                        // TODO: analyse video frame
+                        imageProxy.close()
+                    })
+                }
+
+            // Bind to lifecycle
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, imageAnalysis)
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        },
+        ContextCompat.getMainExecutor(requireContext()))
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        viewBinding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
