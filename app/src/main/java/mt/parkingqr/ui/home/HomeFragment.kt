f16c8bf6544796f8ff7d0df528b5c3a9aa409230
Marek Tews <marek@trx.com.pl>
2023-05-05 16:03:50 +0200

    QRCode scanning from webcam emulator

--- app/src/main/java/mt/parkingqr/ui/home/HomeFragment.kt
+++ app/src/main/java/mt/parkingqr/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package mt.parkingqr.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,11 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import mt.parkingqr.databinding.FragmentHomeBinding
 import java.lang.Exception
 import java.util.concurrent.ExecutorService
@@ -20,10 +26,13 @@ import java.util.concurrent.Executors
 class HomeFragment : Fragment() {
     private var viewBinding: FragmentHomeBinding? = null
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var imageAnalysis: ImageAnalysis
+    private lateinit var barcodeScanner: BarcodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         cameraExecutor = Executors.newSingleThreadExecutor()
+        barcodeScanner = createBarcodeScanner()
     }
 
     override fun onCreateView(
@@ -43,6 +52,15 @@ class HomeFragment : Fragment() {
         return viewBinding!!.root
     }
 
+    private fun createBarcodeScanner(): BarcodeScanner {
+        return BarcodeScanning.getClient(
+            BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+                .build()
+        )
+    }
+
+    @androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
     private fun startCamera(binding: FragmentHomeBinding) {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
 
@@ -60,11 +78,25 @@ class HomeFragment : Fragment() {
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             // Image analysis
-            val imageAnalysis = ImageAnalysis.Builder()
+            imageAnalysis = ImageAnalysis.Builder()
                 .build()
                 .also {
                     it.setAnalyzer(cameraExecutor, ImageAnalysis.Analyzer { imageProxy: ImageProxy ->
                         // TODO: analyse video frame
+//                        Log.d("ImageAnalysis", "Image proxy: format=${imageProxy.format}, w x h=${imageProxy.width} x ${imageProxy.height}, rotation=${imageProxy.imageInfo.rotationDegrees}, timestamp=${imageProxy.imageInfo.timestamp}")
+                        imageProxy.image?.let { mediaImage ->
+                            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                            barcodeScanner.process(image).addOnSuccessListener { barcodes ->
+//                                Log.d("Barcodes", "barcodes size: ${barcodes.size}")
+                                if(barcodes.size == 1) {
+                                    Log.d("Barcodes", "Finally barcode: ${barcodes[0].rawValue}")
+                                    cameraProvider.unbindAll()
+                                    binding.previewView.visibility = View.GONE
+                                    binding.textHome.visibility = View.VISIBLE
+                                    binding.textHome.text = barcodes[0].rawValue
+                                }
+                            }
+                        }
                         imageProxy.close()
                     })
                 }
@@ -83,6 +115,7 @@ class HomeFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        imageAnalysis.clearAnalyzer()
         viewBinding = null
     }
 
